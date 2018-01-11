740e57abcb7ef31072ccb06ac040ee870a4d37bd
Andrei-Alexandru Cartan <andrei-alexandrucartan@Andrei-Alexandrus-MacBook-Pro.local>
2018-01-11 14:14:47 +0000

    Added methods for drawing a vertical line and a right triangle

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -15,4 +15,19 @@ public class Assignment1 {
         }
     }
 
+    public void printVerticalLine(int n){
+        for (int i = 0 ; i < n ; i++){
+            System.out.println("*");
+        }
+    }
+
+    public void printRightTriangle(int n){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,16 @@ public class Main {
         //Horizontal line
         System.out.println("Printing a horizontal line");
         test.printHorizontalLine(5);
+        System.out.println();
+
+        //Vertical line
+        System.out.println("Printing a vertical line");
+        test.printVerticalLine(5);
+        System.out.println();
+
+        //Right Triangle
+        System.out.println("Printing a right triangle");
+        test.printRightTriangle(5);
+        System.out.println();
     }
 }
