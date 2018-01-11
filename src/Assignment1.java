3ceadcc00e436a9f2b94ab838c9f71d1a542c1c8
Andrei-Alexandru Cartan <andrei-alexandrucartan@Andrei-Alexandrus-MacBook-Pro.local>
2018-01-11 15:20:25 +0000

    Created final version of function that draws an isosceles triangle

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -31,14 +31,17 @@ public class Assignment1 {
     }
 
     public void printIsoscelesTriangle(int n){
+        int k = 0;
         for (int i = 1 ; i <= n; i++){
-            for (int j = n ; j >= i; j--){
-                System.out.print(" ");
+            for (int j = 1 ; j <= 2*n-1; j++){
+                if ((j <= n+k) && (j >= n-k)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
             }
-            for (int j = 1; j <= i; j++){
-                System.out.print("*");
-                System.out.print(" ");
-            }
+            k++;
             System.out.println();
         }
     }
